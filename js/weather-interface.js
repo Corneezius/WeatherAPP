885236a4fe19330a6ad08fd07c5d451eda011cf3
Andres Garcia <andresgarciar96@gmail.com>
2016-10-12 12:26:37 -0400

    add working gulp task, bower and npm functionality with API

---
+++ js/weather-interface.js
@@ -0,0 +1,13 @@
+var apiKey = "62543f45c57a30ff4e5a20a40e189756";
+
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $('.showWeather').text("The city you have chosen is " + city + ".");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+      console.log(response);
+    });
+  });
+});
